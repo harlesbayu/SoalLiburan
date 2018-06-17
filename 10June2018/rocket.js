1d783710a87a443e25329928b4eeac111f6772d8
Harles Bayu Anggara <bayuanggaraharles@gmail.com>
2018-06-17 16:03:24 +0700

    Last Update

--- 10June2018/rocket.js
+++ 10June2018/rocket.js
@@ -7,9 +7,28 @@
     - input harus bernilai ganjil , jika tidak ganjil maka " invalid input "
 */
 
-function scotlandFlag(param)
-{
+function scotlandFlag(param) {
     // yout code here 
+    var num = 0;
+    var num1 = param-1;
+    result = ''
+    
+    for (var i = 0; i < param; i++){
+      var asterisk = ''
+      for (var j = 0; j < param; j++){
+        if(j === num || j === num1){
+          asterisk += '*'
+        }
+        else {
+          asterisk += ' '
+        }
+      }
+      num++
+      num1--
+      result += asterisk + '\n'
+      // return asterisk
+    }
+    return result
 }
 
 console.log(scotlandFlag(5))
