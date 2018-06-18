cc1c914940e09b323550d09247a1515991d988f4
Harles Bayu Anggara <bayuanggaraharles@gmail.com>
2018-06-18 17:29:01 +0700

    Last Update

--- 12June2018/003.js
+++ 12June2018/003.js
@@ -1,9 +1,24 @@
-function soal3(param)
-{
+function soal3(param) {
     // your code here
+    var obj = {}
+    var ageGroup = [[],[]]
+      
+    for(var i = 0; i < param.length; i++){
+      if(param[i][1] < 20){
+        ageGroup[0].push(param[i][0])
+      } else {
+        age.push(param[i][0])
+        ageGroup[1].push(param[i][0])
+      }
+    }
+
+    obj.under20 = ageGroup[0]
+    obj.over20 = ageGroup[1]
+    
+    return obj
 }
 
-console.log(soal([
+console.log(soal3([
     ["dimas",17],
     ["akbar",28],
     ["rifki",30],
@@ -13,7 +28,7 @@ console.log(soal([
 
 /*
     {
-        under20:["dimas",'dimitri','saitama'],
-        over20:["akbar","rifki"]
+        under20:['dimas','dimitri','saitama'],
+        over20:['akbar','rifki']
     }
 */
